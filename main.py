1f2a60ab6b725c22a3adb0fe56fa7d632a5adb21
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-09-06 07:26:08 +0000

    added scrape functions

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("AI Web Scraper")
+
+url=st.text_input("Enter a website URL: ")
+
+if st.button("Scrape site"):
+    st.write("Scrapping")
---
+++ scrape.py
@@ -0,0 +1,20 @@
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.service import service
+from time
+def scrape_website(website):
+    print("Connecting to Scraping Browser...")
+    
+    chrome_driver_path=""
+    options=webdriver.ChromeOptions()
+    driver=webdriver.Chrome(service=Service(chrome_driver_path),options=options)
+
+    try:
+        driver.get(website)
+        print("page loaded")
+        html=driver.page_source
+        time.sleep()
+
+        return html
+    
+    finally:
+        driver.quit()
